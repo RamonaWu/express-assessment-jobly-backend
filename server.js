558444e5bb4dea523a78e7ed0cda21c0d6f562a6
Tiantian Wu <92118374+RamonaWu@users.noreply.github.com>
2023-10-12 16:15:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-const ipAddress = '127.0.0.1'
-
 app.listen(PORT, function () {
-  console.log(`Started on http://${ipAddress}:${PORT}`);
+  console.log(`Started on http://localhost:${PORT}`);
 });
