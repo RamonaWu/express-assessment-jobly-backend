32a9d6bb7f4b31b5f014003f15b3468a71fecc25
Tiantian Wu <92118374+RamonaWu@users.noreply.github.com>
2023-10-12 15:37:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const { PORT } = require("./config");
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
+
+// Check the hostname to make sure that it is valid
+const hostname = "base";
+
+// Check if the DNS server is responding
+const dnsResolver = require('dns').getResolver();
+
+dnsResolver.resolve(hostname, (err, address) => {
+  if (err) {
+    console.log("Error resolving hostname:", err.message);
+  } else {
+    console.log("Hostname resolved to:", address);
+  }
+});
+
+// Check the network configuration for errors
+const networkInterfaces = require('os').networkInterfaces();
+
+for (const interfaceName in networkInterfaces) {
+  const interfaces = networkInterfaces[interfaceName];
+
+  for (const interface of interfaces) {
+    if (interface.family === 'IPv4') {
+      console.log("Interface name:", interfaceName);
+      console.log("Interface address:", interface.address);
+    }
+  }
+}
